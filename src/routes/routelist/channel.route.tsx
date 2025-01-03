ddab78d3e91be174d018e2e78768ec4cf6dcd78a
lahirusb97 <lahirushirant@gmail.com>
2025-01-03 06:35:12 +0530

    channel.route created

---
+++ src/routes/routelist/channel.route.tsx
@@ -0,0 +1,12 @@
+import { RouteObject } from "react-router";
+
+export const channelRoutes: RouteObject[] = [
+  {
+    path: "",
+    element: <></>,
+  },
+  {
+    path: "doctor",
+    element: <></>,
+  },
+];
