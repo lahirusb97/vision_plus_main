91284a4d88bd5ebebc462c7166dfa5ac7989fbd0
lahirusb97 <lahirushirant@gmail.com>
2025-01-11 05:30:09 +0530

    normal invoice Ui check

--- src/view/transaction/NormalInvoice.tsx
+++ src/view/transaction/NormalInvoice.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Box, Typography, Paper, TextField, MenuItem, Button, Radio, RadioGroup, FormControlLabel } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Paper,
+  TextField,
+  MenuItem,
+  Button,
+} from "@mui/material";
 import { IconButton } from "@mui/material";
 import AddIcon from "@mui/icons-material/AddBox";
-import CashInput from './CashInput';
-import CardInput from './CardInput';
-
-
+import CashInput from "../../components/inputui/CashInput";
+import CardInput from "../../components/inputui/CardInput";
 
 const TransactionUI = () => {
   const lensOptions = ["Option 1", "Option 2", "Option 3"]; // Replace with actual options
@@ -20,192 +25,191 @@ const TransactionUI = () => {
       }}
     >
       {/* Row 1: Name, Phone No, Address, Sales Staff Code */}
-      <Box sx={{ display: "flex", gap: 2, marginBottom: 2,marginLeft:4, marginRight:4 }}>
-        <Paper sx={{ flex: 1, padding: 2, width:"100px", height:"30px" }}>
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+          marginBottom: 2,
+          marginLeft: 4,
+          marginRight: 4,
+        }}
+      >
+        <Paper sx={{ flex: 1, padding: 2, width: "100px", height: "30px" }}>
           <Typography>Name</Typography>
         </Paper>
-        <Paper sx={{ flex: 1, padding: 2, width:"100px", height:"30px"}}>
+        <Paper sx={{ flex: 1, padding: 2, width: "100px", height: "30px" }}>
           <Typography>Phone No</Typography>
         </Paper>
-        <Paper sx={{ flex: 1, padding: 2, width:"100px", height:"30px" }}>
+        <Paper sx={{ flex: 1, padding: 2, width: "100px", height: "30px" }}>
           <Typography>Address</Typography>
         </Paper>
-        <Paper sx={{ flex: 1, padding: 2, width:"100px", height:"30px" }}>
+        <Paper sx={{ flex: 1, padding: 2, width: "100px", height: "30px" }}>
           <Typography>Sales Staff Code</Typography>
         </Paper>
       </Box>
 
       {/* Row 2: Lens Cleaner Dropdown and Price */}
-      <Box sx={{ display: "flex", gap: 2, marginBottom: 2, marginLeft:4, marginRight:4 }}>
-        <TextField
-          select
-          label="Lens Cleaner"
-          fullWidth
-          sx={{ flex: 1 }}
-        >
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+          marginBottom: 2,
+          marginLeft: 4,
+          marginRight: 4,
+        }}
+      >
+        <TextField select label="Lens Cleaner" fullWidth sx={{ flex: 1 }}>
           {lensOptions.map((option, index) => (
             <MenuItem key={index} value={option}>
               {option}
             </MenuItem>
           ))}
         </TextField>
-        <Paper sx={{ flex: 1, padding: 2,  }}>
+        <Paper sx={{ flex: 1, padding: 2 }}>
           <Typography>Price</Typography>
         </Paper>
       </Box>
       <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        
-      }}
-    >
-      <IconButton
-      sx={{
-        backgroundColor: "white",
-        color: "black",
-        "&:hover": {
-          backgroundColor: "white",
-        },
-      }}
-      size="large"
-      aria-label="add"
-    >
-      <AddIcon />
-    </IconButton>
-    </Box>
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <IconButton
+          sx={{
+            backgroundColor: "white",
+            color: "black",
+            "&:hover": {
+              backgroundColor: "white",
+            },
+          }}
+          size="large"
+          aria-label="add"
+        >
+          <AddIcon />
+        </IconButton>
+      </Box>
 
       {/* Row 3: Full Amount, Discount, First Payment, Balance */}
-      
-    <Box
-      sx={{
-        padding: 4,
-        maxWidth: 2000,
-        margin: "0 auto",
-        borderRadius: 2,
-        display: "flex",
-        flexDirection: "column",
-        gap: 1,
-      }}
-    >
-      {/* Full Amount */}
-      <Paper
+
+      <Box
         sx={{
+          padding: 4,
+          maxWidth: 2000,
+          margin: "0 auto",
+          borderRadius: 2,
           display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: 2,
-          width:"1000px",
-          
+          flexDirection: "column",
+          gap: 1,
         }}
       >
-        <Typography sx={{ fontSize: 16,   }}>Full Amount</Typography>
-        <TextField
-          type="number"
-          value="1000"
-          InputProps={{
-            disableUnderline: true,
-            readOnly: true,
+        {/* Full Amount */}
+        <Paper
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: 2,
+            width: "1000px",
           }}
-          variant="standard"
-          sx={{ textAlign: "right", maxWidth: 150,  }}
-        />
-      </Paper>
+        >
+          <Typography sx={{ fontSize: 16 }}>Full Amount</Typography>
+          <TextField
+            type="number"
+            value="1000"
+            InputProps={{
+              disableUnderline: true,
+              readOnly: true,
+            }}
+            variant="standard"
+            sx={{ textAlign: "right", maxWidth: 150 }}
+          />
+        </Paper>
 
-      {/* Discount */}
-      <Paper
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: 2,
-        }}
-      >
-        <Typography sx={{ fontSize: 16 }}>Discount</Typography>
-        <TextField
-          type="number"
-          value="1000"
-          InputProps={{
-            disableUnderline: true,
-            readOnly: true,
+        {/* Discount */}
+        <Paper
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: 2,
           }}
-          variant="standard"
-          sx={{ textAlign: "right", maxWidth: 150 }}
-        />
-      </Paper>
+        >
+          <Typography sx={{ fontSize: 16 }}>Discount</Typography>
+          <TextField
+            type="number"
+            value="1000"
+            InputProps={{
+              disableUnderline: true,
+              readOnly: true,
+            }}
+            variant="standard"
+            sx={{ textAlign: "right", maxWidth: 150 }}
+          />
+        </Paper>
 
-      {/* First Payment */}
-      <Paper
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: 2,
-        }}
-      >
-        <Typography sx={{ fontSize: 16 }}>First Payment</Typography>
-        <TextField
-          type="number"
-          value="1000"
-          InputProps={{
-            disableUnderline: true,
-            readOnly: true,
+        {/* First Payment */}
+        <Paper
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: 2,
           }}
-          variant="standard"
-          sx={{ textAlign: "right", maxWidth: 150 }}
-        />
-      </Paper>
+        >
+          <Typography sx={{ fontSize: 16 }}>First Payment</Typography>
+          <TextField
+            type="number"
+            value="1000"
+            InputProps={{
+              disableUnderline: true,
+              readOnly: true,
+            }}
+            variant="standard"
+            sx={{ textAlign: "right", maxWidth: 150 }}
+          />
+        </Paper>
 
-      {/* Balance */}
-      <Paper
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: 2,
-        }}
-      >
-        <Typography sx={{ fontSize: 16 }}>Balance</Typography>
-        <TextField
-          type="number"
-          value="1000"
-          InputProps={{
-            disableUnderline: true,
-            readOnly: true,
+        {/* Balance */}
+        <Paper
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: 2,
           }}
-          variant="standard"
-          sx={{ textAlign: "right", maxWidth: 150 }}
-        />
-      </Paper>
-    </Box>
-  
-
-
+        >
+          <Typography sx={{ fontSize: 16 }}>Balance</Typography>
+          <TextField
+            type="number"
+            value="1000"
+            InputProps={{
+              disableUnderline: true,
+              readOnly: true,
+            }}
+            variant="standard"
+            sx={{ textAlign: "right", maxWidth: 150 }}
+          />
+        </Paper>
+      </Box>
 
       {/* Row 4: Payment Methods (Cash and Card) */}
 
-      
-      
+      <Box display="flex" flexDirection="row" gap={10} marginLeft="50px">
+        <CashInput />
+        <CardInput />
+      </Box>
 
-
-    <Box display="flex" flexDirection="row" gap={10} marginLeft="50px">
-    
-      <CashInput/>
-      <CardInput/>
-    </Box>
- 
-
-
-
-
-    
-
-
-     
       {/* Save Button */}
-      <Box sx={{ display: "flex", justifyContent: "right", marginRight:"30px" }}>
-        <Button variant="contained" color="secondary" sx={{ backgroundColor: "#D4B4DC" }}>
+      <Box
+        sx={{ display: "flex", justifyContent: "right", marginRight: "30px" }}
+      >
+        <Button
+          variant="contained"
+          color="secondary"
+          sx={{ backgroundColor: "#D4B4DC" }}
+        >
           Save
         </Button>
       </Box>
