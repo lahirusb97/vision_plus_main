18495974a8049798bbd11f4cf8bdb522ea7f0ef5
lahirusb97 <lahirushirandev@gmail.com>
2025-02-27 20:47:24 +0530

    transaction 1.1v

--- src/validations/factoryInvoiceSchema.ts
+++ src/validations/factoryInvoiceSchema.ts
@@ -1,22 +1,97 @@
 import * as Yup from "yup";
 
 export const factoryInvoiceSchema = Yup.object().shape({
-  sales_staff_code: Yup.string(),
-  hb_rx_right_dist: Yup.string(),
-  hb_rx_left_dist: Yup.string(),
-  hb_rx_right_near: Yup.string(),
-  hb_rx_left_near: Yup.string(),
-  auto_ref_right: Yup.string(),
-  auto_ref_left: Yup.string(),
-  right_eye_dist_sph: Yup.string(),
-  right_eye_dist_cyl: Yup.string(),
-  right_eye_dist_axis: Yup.string(),
-  right_eye_near_sph: Yup.string(),
-  left_eye_dist_sph: Yup.string(),
-  left_eye_dist_cyl: Yup.string(),
-  left_eye_dist_axis: Yup.string(),
-  left_eye_near_sph: Yup.string(),
-  remark: Yup.string(),
+  sales_staff_code: Yup.string().required("Enter Saff Code"),
+  hb_rx_right_dist: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  hb_rx_left_dist: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  hb_rx_right_near: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  hb_rx_left_near: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  auto_ref_right: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  auto_ref_left: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  right_eye_dist_sph: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  right_eye_dist_cyl: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  right_eye_dist_axis: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  right_eye_near_sph: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  left_eye_dist_sph: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  left_eye_dist_cyl: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  left_eye_dist_axis: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  left_eye_near_sph: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
+  remark: Yup.string()
+    .nullable()
+    .strict()
+    .transform((value, originalValue) =>
+      String(originalValue).trim() === "" ? null : value
+    ),
   name: Yup.string(),
   nic: Yup.string(),
   phone_number: Yup.string(),
