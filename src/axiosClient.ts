f2006134c882d070ade628699d35ef27f5523c4f
chalani <cchalaninavo7@gmail.com>
2025-03-03 16:42:20 +0530

    comit

--- src/axiosClient.ts
+++ src/axiosClient.ts
@@ -9,8 +9,8 @@ import { getTokenFromLocalStorage } from "./utils/geAuthToken";
 // Create an Axios instance with a base URL
 const axiosClient: AxiosInstance = axios.create({
   // baseURL: import.meta.env.VITE_BASE_URL,
-  baseURL: "http://193.203.161.90:8000/api",
-  // baseURL: "http://127.0.0.1:8005/api",
+  //baseURL: "http://193.203.161.90:8000/api",
+  baseURL: "http://127.0.0.1:8005/api",
   withCredentials: false,
 });
 
--- src/view/transaction/factory_invoice/InvoiceView.tsx
+++ src/view/transaction/factory_invoice/InvoiceView.tsx
@@ -10,24 +10,21 @@ import {
   Button,
   TableHead,
   CircularProgress,
-  Chip,
 } from "@mui/material";
 import { useLocation } from "react-router";
 import { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import useGetSingleInvoiceDetail from "../../../hooks/useGetSingleInvoiceDetail";
-import { Check } from "@mui/icons-material";
 import log from "../../../assets/defalt/Rectangle 522.png";
+
 const InvoiceView = () => {
   const location = useLocation();
-  // const { lense, frame, order, patient } = location.state || {};
   const queryParams = new URLSearchParams(location.search);
   const componentRef = useRef(null);
   const reactToPrintFn = useReactToPrint({ contentRef: componentRef });
   const { invoiceDetail, invoiceDetailLoading } = useGetSingleInvoiceDetail(
     parseInt(queryParams.get("order_id") ?? "")
   );
-  console.log(invoiceDetail);
 
   const DateView = (date: string) => {
     return new Date(date).toLocaleString("default", {
@@ -39,6 +36,7 @@ const InvoiceView = () => {
       hour12: true,
     });
   };
+
   if (invoiceDetailLoading) {
     return (
       <Box
@@ -59,15 +57,25 @@ const InvoiceView = () => {
       </Typography>
     );
   }
+
   return (
     <div>
+      {/* A5 Paper Size Container */}
       <Box
         sx={{
           p: 4,
-          maxWidth: 700,
-          margin: "auto",
-          border: "1px solid black",
-          marginTop: "20px",
+          width: "210mm", // A5 width
+          minHeight: "148mm", // A5 height
+          margin: "0 auto",
+          border: "1px solid #000",
+          fontFamily: 'Arial, sans-serif',
+          "@media print": {
+            width: "210mm",
+            minHeight: "148mm",
+            border: "none",
+            margin: "0",
+            padding: "5mm",
+          },
         }}
         ref={componentRef}
       >
@@ -118,7 +126,6 @@ const InvoiceView = () => {
             <Typography variant="body2">No: 34, Aluthgama Road</Typography>
             <Typography variant="body2">Mathugama</Typography>
             <Typography variant="body2">Sri Lanka</Typography>
-            <Typography variant="body2">November 05, 2024</Typography>
           </Box>
         </Box>
 
@@ -126,49 +133,29 @@ const InvoiceView = () => {
         <TableContainer component={Paper}>
           <Table
             size="small"
-            sx={{ minWidth: 700, maxWidth: "1200px" }}
+            sx={{ width: "100%" }}
             aria-label="spanning table"
           >
             <TableHead>
               <TableRow>
-                <TableCell>Desc</TableCell>
-                <TableCell align="right">Qty.</TableCell>
+                <TableCell>Item Name</TableCell>
+                <TableCell align="right">Qty</TableCell>
                 <TableCell align="right">Unit</TableCell>
-                <TableCell align="right">Sum</TableCell>
+                <TableCell align="right">Value</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {/* Item List */}
               {invoiceDetail?.order_items.map((row, index) => (
-                <TableRow key={index} hover>
+                <TableRow key={index}>
                   <TableCell>
-                    <Box display="flex" alignItems="center">
-                      {row.lens ? (
-                        <Check style={{ marginRight: 8 }} />
-                      ) : row.frame ? (
-                        <Check style={{ marginRight: 8 }} />
-                      ) : (
-                        <Check style={{ marginRight: 8 }} />
-                      )}
-                      <Typography variant="body1">
-                        {row.lens
-                          ? row?.lense_name
-                          : row.frame
-                          ? row?.frame_name
-                          : ""}
-                      </Typography>
-                    </Box>
-                  </TableCell>
-                  <TableCell align="right">{row.quantity}</TableCell>
-                  <TableCell align="right">
-                    <Typography variant="subtitle1">
-                      {" "}
-                      {row.price_per_unit}
+                    <Typography variant="body1">
+                      {row.lens ? row?.lense_name : row.frame ? row?.frame_name : ""}
                     </Typography>
                   </TableCell>
-                  <TableCell align="right">
-                    <Typography variant="subtitle1"> {row.subtotal}</Typography>
-                  </TableCell>
+                  <TableCell align="right">{row.quantity}</TableCell>
+                  <TableCell align="right">{row.price_per_unit}</TableCell>
+                  <TableCell align="right">{row.subtotal}</TableCell>
                 </TableRow>
               ))}
 
@@ -205,45 +192,49 @@ const InvoiceView = () => {
                   </Typography>
                 </TableCell>
               </TableRow>
-
-              {/* Payment Details */}
-              {invoiceDetail.order_payments.map((payment) => (
-                <TableRow key={payment.id} hover>
-                  <TableCell align="right" colSpan={2}>
-                    <Box
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="flex-end"
-                    >
-                      <Typography variant="body2" style={{ marginRight: 8 }}>
-                        {DateView(payment.payment_date)}
-                      </Typography>
-                      <Chip label={payment.payment_method} />
-                    </Box>
-                  </TableCell>
-                  <TableCell align="right">
-                    <Typography variant="subtitle1">
-                      {payment.amount}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-              ))}
+              <TableRow>
+                <TableCell align="right" colSpan={2}>
+                  <Typography variant="subtitle1">Cash/Card</Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="subtitle1">
+                    {invoiceDetail.order_payments[0]?.amount}
+                  </Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align="right" colSpan={2}>
+                  <Typography variant="subtitle1">Balance</Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography variant="subtitle1">
+                    {invoiceDetail.order_details.total_price - invoiceDetail.order_payments[0]?.amount}
+                  </Typography>
+                </TableCell>
+              </TableRow>
             </TableBody>
           </Table>
         </TableContainer>
+
+        {/* Branches Section */}
+        <Box sx={{ mt: 3 }}>
+          <Typography sx={{ mt: 3 }} variant="h6">Our Branches</Typography>
+          <Typography sx={{ mt: 2 }}variant="body1">Mathugama Branch</Typography>
+          <Typography variant="body1">Tel: 0342247354</Typography>
+          <Typography sx={{ mt: 2 }} variant="body1">Aluthgama Branch</Typography>
+          <Typography variant="body1">Tel: 0342275268</Typography>
+        </Box>
 
         {/* Footer Note */}
         <Typography
           variant="body2"
           align="center"
-          sx={{ mt: 3, fontWeight: "Bold" }}
+          sx={{ mt: 3, fontWeight: "bold" }}
         >
-          <strong>
-            {" "}
-            We will not be responsible for any uncollected orders after 3 months
-            * Non-refundable{" "}
-          </strong>
-          Software develop by Furigen technology
+          We will not be responsible for any uncollected orders after 3 months * Non-refundable
+        </Typography>
+        <Typography variant="body2" align="center">
+          Software developed by Furigen technology
         </Typography>
       </Box>
       <Button
