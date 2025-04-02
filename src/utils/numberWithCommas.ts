60f19f46f3bb76b07fe3560898611354fca8cacd
lahirusb97 <lahirushirandev@gmail.com>
2025-04-02 11:31:16 +0530

    number comma updated

--- src/utils/numberWithCommas.ts
+++ src/utils/numberWithCommas.ts
@@ -2,6 +2,9 @@ export function numberWithCommas(value: number) {
   // Convert value to an integer
   if (typeof value == "number") {
     return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  } else if (typeof value == "string") {
+    const parsedValue = parseInt(value);
+    return parsedValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
   if (isNaN(value)) return "Invalid number";
