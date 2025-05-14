98bc2a98ea60fa1a55a7d77156c792601f5e3d1e
lahirusb97 <lahirushirandev@gmail.com>
2025-05-14 20:54:07 +0530

    update axios

--- src/axiosClient.ts
+++ src/axiosClient.ts
@@ -9,12 +9,12 @@ import { getUserAuth } from "./utils/authDataConver";
 // Create an Axios instance with a base URL
 const axiosClient: AxiosInstance = axios.create({
   // baseURL: import.meta.env.VITE_BASE_URL,
-  // baseURL: "https://www.onlineict.site/api/",
-  baseURL:
-    import.meta.env.VITE_DEV === "true"
-      ? import.meta.env.VITE_LOCAL_BASE_URL
-      : import.meta.env.VITE_API_BASE_URL,
-  withCredentials: false,
+  baseURL: "https://www.onlineict.site/api/",
+  // baseURL:
+  //   import.meta.env.VITE_DEV === "true"
+  //     ? import.meta.env.VITE_LOCAL_BASE_URL
+  //     : import.meta.env.VITE_API_BASE_URL,
+  // withCredentials: false,
 });
 
 // Request interceptor to add Authorization header
